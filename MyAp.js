af8a3cd1ca177d58ef2660833281d4c7688dca45
Gill2412 <gurjindergill8@gmail.com>
2019-12-04 23:10:21 +0000

    change in line 64

--- MyAp.js
+++ MyAp.js
@@ -61,7 +61,7 @@ router.post('/post/json', function(req, res) {
     xmlFileToJs('Salon.xml', function(err, result) {
       if (err) throw (err);
       //This is where you pass on information from the form inside index.html in a form of JSON and navigate through our JSON (XML) file to create a new entree object
-      result.cafemenu.section[obj.sec_n].entree.push({'Client': obj.Client, 'Time': obj.Time}); //If your XML elements are differet, this is where you have to change to your own element names
+      result.Appointments.Days[obj.sec_n].Details.push({'Client': obj.Client, 'Time': obj.Time}); //If your XML elements are differet, this is where you have to change to your own element names
       //Converting back to our original XML file from JSON
       jsToXmlFile('PaddysCafe.xml', result, function(err) {
         if (err) console.log(err);
